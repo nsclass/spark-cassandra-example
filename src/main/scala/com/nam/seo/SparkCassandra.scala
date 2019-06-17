d2241232e1416311a91c66066a95e4937999e441
Nam Seob Seo <nsclass@gmail.com>
2019-06-16 19:02:06 -0500

    using spark session.

--- src/main/scala/com/nam/seo/SparkCassandra.scala
+++ src/main/scala/com/nam/seo/SparkCassandra.scala
@@ -1,7 +1,8 @@
 package com.nam.seo
 
-import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
+import org.apache.spark.sql.cassandra._
+import org.apache.spark.sql.{SparkSession}
 
 /**
   * Date 2019-06-15 
@@ -11,14 +12,15 @@ import com.datastax.spark.connector._
 
 object SparkCassandra {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf()
-      .setMaster("local")
-      .setAppName("SparkCassandra")
-      .set("spark.cassandra.connection.host", "127.0.0.1")
+    val spark = SparkSession.builder()
+      .appName("SparkCassandra")
+      .master("local")
+      .config("spark.cassandra.connection.host", "127.0.0.1")
+      .getOrCreate()
 
-    val sc = new SparkContext(conf)
-    val rdd = sc.cassandraTable(keyspace = "system", table = "local")
-        .select("key", "cluster_name", "cql_version")
+    // Read data as RDD
+    val rdd = spark.sparkContext.cassandraTable(keyspace = "system", table = "local")
+      .select("key", "cluster_name", "cql_version")
 
     println("Data read as RDD")
     rdd.collect()
@@ -26,6 +28,18 @@ object SparkCassandra {
         println(row.getString("key"))
         println(row.getString("cluster_name"))
         println(row.getString("cql_version"))
+      })
+
+    // Read data as DataSet (DataFrame)
+    val dataset = spark.read
+      .cassandraFormat(keyspace = "system", table = "local")
+      .load()
+
+    dataset.collect()
+      .foreach(row => {
+        println(row.getAs("key"))
+        println(row.getAs("cluster_name"))
+        println(row.getAs("cql_version"))
       })
   }
 }
